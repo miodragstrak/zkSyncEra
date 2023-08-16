1a91778d962ab3fa52270c83807c22b3b560e7e0
miodragstrak <44975850+miodragstrak@users.noreply.github.com>
2023-08-16 22:13:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+functions: {
+    initializeProviderAndSigner() {
+      // TODO: initialize provider and signer based on `window.ethereum`
+    },
+  
+    async getGreeting() {
+      // TODO: return the current greeting
+      return "";
+    },
+  
+    async getFee() {
+      // TODO: return formatted fee
+      return "";
+    },
+  
+    async getBalance() {
+      // Return formatted balance
+      return "";
+    },
+    async getOverrides() {
+      if (this.selectedToken.l1Address != ETH_L1_ADDRESS) {
+        // TODO: Return data for the paymaster
+      }
+  
+      return {};
+    },
+  
